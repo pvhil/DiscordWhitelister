3dfb7eeccdb364b5772f50facb76205a720beb53
pvhil <54856838+pvhil@users.noreply.github.com>
2020-10-24 18:49:09 +0200

    Update FileManager.java

--- src/main/java/pvhil/whitelister/FileManager.java
+++ src/main/java/pvhil/whitelister/FileManager.java
@@ -4,7 +4,7 @@ public class FileManager {
     private static FileWriter fileWriter = new FileWriter(main.getTutorial().getDataFolder().getPath(), "config.yml");
 
     public static void loadFile() {
-        setValue("settings.discordkey", "einsetzen");
+        setValue("settings.discordkey", "BOTTOKEN");
     }
 
     private static void setValue(final String valuePath, final String value){
