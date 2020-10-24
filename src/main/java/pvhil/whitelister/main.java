e41feecd3983dfee2bf66cd7062d0164bfae9d4d
BuildTools <unconfigured@null.spigotmc.org>
2020-10-24 19:26:19 +0200

    update

--- src/main/java/pvhil/whitelister/main.java
+++ src/main/java/pvhil/whitelister/main.java
@@ -6,15 +6,17 @@ import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import pvhil.whitelister.other.Metrics;
 
 import javax.security.auth.login.LoginException;
 
 import static net.dv8tion.jda.api.JDABuilder.createDefault;
 
-public class main extends JavaPlugin implements EventListener {
+public class main extends JavaPlugin implements EventListener, Listener {
     private JDA discordBot;
     public String name;
 
@@ -22,6 +24,11 @@ public class main extends JavaPlugin implements EventListener {
 
     @Override
     public void onEnable() {
+        int pluginId = 9197; // <-- Replace with the id of your plugin!
+        Metrics metrics = new Metrics(this, pluginId);
+
+        // Optional: Add custom charts
+        metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
         tutorial = this;
         name = (String) FileManager.getValue("settings.discordkey");
         FileManager.loadFile();
@@ -38,14 +45,13 @@ public class main extends JavaPlugin implements EventListener {
         return tutorial;
     }
     @EventHandler
-    public void onPlayerJoin(PlayerLoginEvent event) throws InterruptedException {
+    public void onPlayerJoin(PlayerLoginEvent event) {
         discordBot.getPresence().setActivity(Activity.watching(Bukkit.getServer().getOnlinePlayers().size()+" Spieler sind online!"));
 
     }
     @Override
     public void onDisable() {
         System.out.println("Plugin shutdown (discord)");
-        return;
     }
 
     @Override
